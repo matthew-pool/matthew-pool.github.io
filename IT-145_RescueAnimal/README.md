14c1b4293465962b19a35003e0e5fe194e1c7897
Matthew Pool <matthew.pool@snhu.edu>
2024-11-22 15:03:24 -0600

    program revisions and YouTube URL

---
+++ IT-145_RescueAnimal/README.md
@@ -0,0 +1,15 @@
+# To Run: 
+1. Install JRE or JDK
+2. Add the JDK or JRE bin folder to PATH
+3. Download 'Rescue Animal.zip'
+4. Extract 'Rescue Animal.zip'
+5. Execute run.bat in the 'RescueAnimal' directory
+
+# Rescue Animal (Java)
+This is a program to keep track of a list of  animals. The user can add dogs and monkeys and their associated attributes to storage. The user may also print various lists if desired.
+
+![pic1](images/Picture1.png)
+![pic2](images/Picture2.png)
+![pic3](images/Picture3.png)
+![pic4](images/Picture4.png)
+![pic5](images/Picture5.png)
---
+++ IT-145_RescueAnimal/rescue_animal_v4.0/src/test/java/rescueAnimal/DogTest.java
@@ -0,0 +1,65 @@
+package rescueAnimal;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests for the Dog class.
+ *
+ * <p>Tests class functions and instantiations.</p>
+ *
+ * @author Matthew Pool
+ * @version 3.0
+ * @since 2024-11-24
+ */
+public class DogTest {
+
+    @Test
+    @DisplayName("Test default constructor.")
+    public void testDefaultConstructor() {
+        Dog dog = new Dog();
+        assertEquals(null, dog.getName());
+    }
+
+    @Test
+    @DisplayName("Test parameterized constructor.")
+    public void testParameterizedConstructor() {
+        Dog dog = new Dog("Crimson", "Great Dane", "male", "3", "35.2", "02-03-2020",
+                "United States", "Phase 1", false, "United States");
+        assertEquals("Crimson", dog.getName());
+    }
+
+    @Test
+    @DisplayName("Test dog setters and getters.")
+    public void testDogGetters() {
+        Dog dog = new Dog();
+
+        // Setters
+        dog.setName("Sophie");
+        dog.setAnimalType("dog");
+        dog.setBreed("German Shepherd");
+        dog.setGender("male");
+        dog.setAge("1");
+        dog.setWeight("25.6");
+        dog.setAcquisitionDate("05-12-2019");
+        dog.setAcquisitionCountry("United States");
+        dog.setTrainingStatus("intake");
+        dog.setReserved(false);
+        dog.setInServiceCountry("United States");
+
+        // Getters
+        assertEquals("Sophie", dog.getName());
+        assertEquals("dog", dog.getAnimalType());
+        assertEquals("German Shepherd", dog.getBreed());
+        assertEquals("male", dog.getGender());
+        assertEquals("1", dog.getAge());
+        assertEquals("25.6", dog.getWeight());
+        assertEquals("05-12-2019", dog.getAcquisitionDate());
+        assertEquals("United States", dog.getAcquisitionCountry());
+        assertEquals("intake", dog.getTrainingStatus());
+        assertEquals(false, dog.getReserved());
+        assertEquals("United States", dog.getInServiceCountry());
+    }
+}
