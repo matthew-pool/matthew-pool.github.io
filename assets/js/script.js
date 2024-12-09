44bf385abdc8a79c08eb66148376c6c2dad7276f
Matthew Pool <matthew.pool@snhu.edu>
2024-12-09 14:51:29 -0600

    switched to using floating navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,1 @@
-document.querySelector('.go-to-top').addEventListener('click', function (e) {
-    e.preventDefault(); // Prevents default jump
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+
